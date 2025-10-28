6919beffacffb87df5d34542dd5daa51b30aad52
trungduongmew <duongtrungduongmew.com>
2025-10-28 15:57:35 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const port = 8080;
 
-// Đọc thông tin kết nối từ Biến Môi Trường
+
 const dbConfig = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -12,24 +12,24 @@ const dbConfig = {
     port: process.env.DB_PORT || 5432,
 };
 
-// Tạo pool kết nối
+
 const pool = new Pool(dbConfig);
 
 const server = http.createServer(async (req, res) => {
     try {
-        // Thử kết nối và truy vấn
+        
         const client = await pool.connect();
-        const result = await client.query('SELECT NOW()'); // Truy vấn thời gian
+        const result = await client.query('SELECT NOW()'); 
         const time = result.rows[0].now;
         client.release();
 
-        // Trả về nếu thành công
+        
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(`CICD  DATABASE THANH CONG!\n\nThoi gian DB server: ${time}\n`);
+        res.end(`CICD Test DATABASE THANH CONG!\n\nThoi gian DB server: ${time}\n`);
 
     } catch (err) {
-        // Trả về nếu thất bại
+        
         console.error('Loi ket noi DB:', err.message);
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/plain');
